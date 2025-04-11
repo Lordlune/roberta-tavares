451fbda81aa27e616475f72aba46397e12c85689
NB-001389\Colaborador <patryck.araujo@extreme.digital>
2025-04-11 00:22:20 -0300

    construção 1.0 da landing page roberta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'roberta-taveres';
+  email = 'nakupendda@gmail.com'
+  telefone = '21967469926'
+
+  waMe(){
+    let url = 'https://wa.me/21967469926?text=Olá Roberta, tudo bem? Gostaria de mais informações!'
+    window.open(url, '_blank')
+  }
 }
